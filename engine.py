83f894982ce6142468c59de01f36b0417581f52c
onlyonebean <146683948+onlyonebean@users.noreply.github.com>
2023-10-14 18:03:56 -0700

    Folder Directory Charnges

--- engine.py
+++ engine.py
@@ -1,44 +1,45 @@
-import mysql.connector
-
-# Create a database connection
-db = mysql.connector.connect(
-    host="localhost:3306",
-    user="Nona-Pahia",
-    password="9271",
-    database="study"
-)
-
-
-from flask import Flask, request, jsonify
+from flask import Flask, request, render_template, redirect, url_for
 import mysql.connector
 
 app = Flask(__name__)
 
-# Configure MySQL connection
-db = mysql.connector.connect(
-    host="localhost",
-    user="your_username",
-    password="your_password",
-    database="TEST"
-)
-
-# Create a cursor for database operations
-cursor = db.cursor()
+# Configure your MySQL connection
+db = {
+    "host":"Nona-Pahia",
+    "user":"Fiona",
+    "password":"9271",
+    "database":"STUDY"
+}
 
 @app.route('/add_flashcard', methods=['POST'])
 def add_flashcard():
-    data = request.get_json()
-    subject = data['subject']
-    question = data['question']
-    answer = data['answer']
+    if request.method == 'POST':
+        # Get flashcard data from the form
+        subject = request.form['subject']
+        question = request.form['question']
+        answer = request.form['answer']
 
-    # Insert the flashcard data into the database
-    insert_query = "INSERT INTO flashcards (subject, question, answer) VALUES (%s, %s, %s)"
-    cursor.execute(insert_query, (subject, question, answer))
-    db.commit()
+        # Insert the flashcard data into the database
+        cursor = db.cursor()
+        cursor.execute("INSERT INTO flashcards (subject, question, answer) VALUES (%s, %s, %s)",
+                       (subject, question, answer))
+        db.commit()
+        cursor.close()
 
-    return jsonify({'message': 'Flashcard added successfully'})
+        return redirect(url_for('main_page'))  # Redirect back to the main page
+
+@app.route('/all_flashcards')
+def all_flashcards():
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM flashcards")
+    flashcards = cursor.fetchall()
+    cursor.close()
+    
+    return render_template('all_flashcards.html', flashcards=flashcards)
+
+@app.route('/')
+def main_page():
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run()
-
